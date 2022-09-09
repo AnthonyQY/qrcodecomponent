d1587e342eb4f7aba7648bced575da365798eddf
AnthonyQY <contact@lolitek.com>
2022-09-09 02:53:17 -0700

    Fixed HTML tag being on wrong element

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,8 +2,8 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html>
-      <Head lang="en">
+    <Html lang="en">
+      <Head>
         <link
           rel="apple-touch-icon"
           sizes="180x180"
